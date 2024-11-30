d577f1c00c0e365744b490822975ddacd2132247
Harshal Ranjhani <ranjhaniharshal@gmail.com>
2024-11-30 22:39:53 +0530

    Initial Commit

---
+++ ClearView/ClearViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearViewApp.swift
+//  ClearView
+//
+//  Created by Harshal Ranjhani on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ClearViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClearViewTests/ClearViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClearViewTests.swift
+//  ClearViewTests
+//
+//  Created by Harshal Ranjhani on 11/30/24.
+//
+
+import Testing
+@testable import ClearView
+
+struct ClearViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
